018218a024549eb105e760a2b54bca130f4d95be
forest611 <forest611328@gmail.com>
2020-03-02 18:32:33 +0900

    Initial commit

---
+++ src/main/java/red/man10/man10voteplugin/Man10VotePlugin.kt
@@ -0,0 +1,11 @@
+package red.man10.man10voteplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10VotePlugin : JavaPlugin() {
+    override fun onEnable() { // Plugin startup logic
+    }
+
+    override fun onDisable() { // Plugin shutdown logic
+    }
+}
